c353f1c20cd73d9c31b718569dfb677722681622
Elaine Ulsh <elaine.ulsh@bsu.edu>
2024-09-09 17:18:28 -0400

    Convert serverMessage to ZonedDateTime and take out Scanner so that we don't have to send a message for it to run.

--- src/Client.java
+++ src/Client.java
@@ -6,7 +6,6 @@ import java.time.Instant;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) throws Exception {
@@ -17,8 +16,7 @@ public class Client {
         InetAddress serverIP = InetAddress.getByName(args[0]);
         int serverPort = Integer.parseInt(args[1]);
 
-        Scanner keyboard = new Scanner(System.in);
-        String message = keyboard.nextLine();
+        String message = "";
 
         DatagramSocket socket = new DatagramSocket();
         DatagramPacket request = new DatagramPacket(
@@ -43,8 +41,7 @@ public class Client {
 
         int value = ByteBuffer.wrap(serverMessage).getInt();
         long unsignedValue = Integer.toUnsignedLong(value);
-
-        ZonedDateTime date = ZonedDateTime.ofInstant(Instant.ofEpochMilli(unsignedValue), ZoneId.systemDefault());
+        ZonedDateTime date = ZonedDateTime.ofInstant(Instant.ofEpochSecond(unsignedValue), ZoneId.systemDefault()).minusYears(70);
         System.out.println(unsignedValue);
         System.out.println(date);
     }
